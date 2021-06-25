e0efbe194fa0e1093dc5b037d5562608e0fba70f
Vishnuprasadha <84711947+Vishnuprasadha@users.noreply.github.com>
2021-06-25 23:15:41 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,1 +1,24 @@
-
+#include <stdio.h>
+void swap_max(int arr[],int n,int index){
+  int max=arr[0];
+  int j=0;
+  for(int i=1;i<n;i++){
+    if(arr[i]>max){
+     
+      j=i;
+  }
+ }
+ int temp=arr[index];
+ arr[index]=arr[j];
+ arr[j]=temp;
+ 
+}
+void ssort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        swap_max(arr,n,i);
+    }
+    
+     for(int i=0;i<n;i++){
+       printf("%d\n",arr[i]);
+    }
+}
